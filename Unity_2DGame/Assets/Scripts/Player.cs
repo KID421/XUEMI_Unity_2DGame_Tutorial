dd0f715867a51f5c8aa3dfddca09e992c7dd01e5
KID <50666207+KID421@users.noreply.github.com>
2020-01-21 13:40:39 +0800

    發佈網頁版本

--- Unity_2DGame/Assets/Scripts/Player.cs
+++ Unity_2DGame/Assets/Scripts/Player.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animator aniPlayer;
     private SpriteRenderer sprPlayer;
+    private float hp = 100;
+    private float hpMax;
+    private Image hpBar;
+    private bool dead;
+    private GameManager gm;
 
     [Header("移動速度"), Range(0, 1000)]
     public float speed = 10;
@@ -29,14 +35,51 @@ public class Player : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 死亡方法：死亡動畫、關閉腳本
+    /// </summary>
+    private void Dead()
+    {
+        aniPlayer.SetTrigger("死亡觸發");    // 動畫.設定觸發("參數名稱")
+        this.enabled = false;               // 關閉腳本
+        dead = true;                        // 已經死亡
+        gm.GameOver();
+    }
+
     private void Start()
     {
         aniPlayer = GetComponent<Animator>();
         sprPlayer = GetComponent<SpriteRenderer>();
+        hpBar = GameObject.Find("血條").GetComponent<Image>();
+        gm = FindObjectOfType<GameManager>();
+
+        hpMax = hp;
     }
 
     private void Update()
     {
         Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (dead) return;                   // 如果 死亡 跳出
+
+        if (collision.tag == "陷阱")        // 如果 碰到物件.標籤 為 "陷阱"
+        {
+            hp -= 20;                       // 血量 -= 20
+            hpBar.fillAmount = hp / hpMax;  // 血條.長度 = 血量 / 血量最大值
+
+            if (hp <= 0) Dead();            // 如果 血量 <= 0 呼叫死亡方法
+        }
+
+        if (collision.tag == "櫻桃")        // 如果 碰到物件.標籤 為 "櫻桃"
+        {
+            hp += 5;                        // 血量 -= 20
+            hp = Mathf.Clamp(hp, 0, hpMax); // 數學.夾住(血量，0，血量最大值)
+            hpBar.fillAmount = hp / hpMax;  // 血條.長度 = 血量 / 血量最大值
+        }
+
+        Destroy(collision.gameObject);      // 刪除(碰到物件.遊戲物件)
     }
 }
